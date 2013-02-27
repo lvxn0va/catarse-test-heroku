03c20be9e404398299d593c5671c98babc9a2757
lvxn0va <lvx.n0va@gmail.com>
2013-02-26 22:22:50 -0800

    Google-Oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,21 +21,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    omniauth = request.env['omniauth.auth']
-    @user = User.
-      select('users.*').
-      joins('JOIN authorizations ON authorizations.user_id = users.id').
-      joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
-      where("authorizations.uid = ? AND oauth_providers.name = 'google_oauth2'", omniauth[:uid]).first
-
-    if @user && @user.persisted?
-      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => "google_oauth2")
-      sign_in @user, :event => :authentication
-      redirect_to(session[:return_to] || root_path)
-      session[:return_to] = nil
-    else
-      session[:omniauth] = omniauth.except('extra')
-      redirect_to new_user_registration_url
-    end
+  omniauth = request.env['omniauth.auth']
+  @user = User.find_for_google_oauth(request.env["omniauth.auth"], current_user)
+  if @user && @user.persisted?
+    flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => "Google")
+    sign_in @user, :event => :authentication #this will throw if @user is not activated
+    redirect_to(session[:return_to] || root_path)
+    session[:return_to] = nil 
+    #set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+  else
+    session[:omniauth] = omniauth.except('extra')
+    #session["devise.oauth_data"] = request.env["omniauth.auth"]["info"]
+    redirect_to new_user_registration_url
   end
+end
 end
