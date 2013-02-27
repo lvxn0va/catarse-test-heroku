d8c11e035caffcdae5b0db3cc878df253e649826
lvxn0va <lvx.n0va@gmail.com>
2013-02-26 19:48:39 -0800

    Google-oauth2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -19,4 +19,23 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def google_oauth2
+    omniauth = request.env['omniauth.auth']
+    @user = User.
+      select('users.*').
+      joins('JOIN authorizations ON authorizations.user_id = users.id').
+      joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
+      where("authorizations.uid = ? AND oauth_providers.name = 'google_oauth2'", omniauth[:uid]).first
+
+    if @user && @user.persisted?
+      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => "google_oauth2")
+      sign_in @user, :event => :authentication
+      redirect_to(session[:return_to] || root_path)
+      session[:return_to] = nil
+    else
+      session[:omniauth] = omniauth.except('extra')
+      redirect_to new_user_registration_url
+    end
+  end
 end
